33dacc8860cb9d0cec2bb37940dbbf08449f3384
AlexisRodriguez <alexisrodriguez.nttdata@latam.com>
2026-05-12 21:50:04 -0400

    feat: fix api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://qa.onmisalud.cl/api/v1', // La URL de tu backend en NestJS
+  apiUrl: 'https://qa.omnisalud.cl/api/v1', // La URL de tu backend en NestJS
   envName: 'Development/QA'
 };
